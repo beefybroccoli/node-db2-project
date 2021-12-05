caa01f20316ed1085f2606c7ea49fcea2d1ce730
beefybroccoli <tedkimemail@yahoo.com>
2021-12-04 21:54:18 -0800

    ...

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,8 @@
-// DO YOUR MAGIC
+const express = require('express');
+const router = express();
+
+router.get("/", (req, res)=>{
+    res.status(200).json({message:"hello world"});
+})
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,7 +1,15 @@
 const express = require("express")
+const routerCars = require("./cars/cars-router");
 
 const server = express()
 
-// DO YOUR MAGIC
+server.use(express.json());
+server.use(express.Router());
+
+server.use("/api/cars", routerCars);
+
+server.get("*", (req, res)=>{
+    res.status(404).json({message:`${req.path} path not found`});
+})
 
 module.exports = server
