63f828c13aae0c59d1cd4336a135b5492860755c
beefybroccoli <tedkimemail@yahoo.com>
2021-12-04 21:46:47 -0800

    created seed and migration

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,12 @@
-// STRETCH
+exports.seed = function(knex, Promise) {
+    return knex('cars').truncate()
+        .then( function(){
+            return knex('cars').insert([
+                {id:1, vin:"toyota1", make:"toyota", model:"corolla",mileage:1000,title:null, transmission:null},
+                {id:2, vin:"ford1", make:"ford", model:"fisa",mileage:1000,title:null, transmission:null},
+                {id:3, vin:"chrysler1", make:"chrysler", model:"coupaa",mileage:1000,title:null, transmission:null},
+                {id:4, vin:"dogdge1", make:"dodge", model:"charger",mileage:1000,title:null, transmission:null},
+                {id:5, vin:"tesla1", make:"tesla", model:"2k",mileage:1000,title:null, transmission:null},
+            ])
+        });
+}
